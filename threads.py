47f12e30fa71b8787045ffb212f2d475f375d487
oskarruo <oskar.ruokolainen@gmail.com>
2022-04-24 23:31:52 +0300

    added delete, edit, and visible check

--- threads.py
+++ threads.py
@@ -1,16 +1,30 @@
 from db import db
 
 def get_threads(topic):
-    sql = "SELECT threads.id, threads.threadname, count(messages), max(sent_at) FROM threads, topics, messages WHERE topics.topic=:topic AND topics.id = threads.topic_id AND messages.thread_id = threads.id AND messages.visible > 0 GROUP BY threads.id;"
+    sql = "SELECT threads.id, threads.threadname, count(messages), max(sent_at) FROM threads, topics, messages WHERE topics.topic=:topic AND topics.id = threads.topic_id AND messages.thread_id = threads.id AND messages.visible > 0 AND threads.visible > 0 GROUP BY threads.id;"
     return db.session.execute(sql, {"topic":topic}).fetchall()
     
 def get_threadinfo(thread_id):
-    sql = "SELECT id, threadname FROM threads WHERE id=:thread_id;"
+    sql = "SELECT id, threadname, created_by, name_modified FROM threads WHERE id=:thread_id;"
     return db.session.execute(sql, {"thread_id":thread_id}).fetchone()
 
 def create_thread(topic_id, user, threadname):
-    sql = "INSERT INTO threads(topic_id, created_by, threadname, created_at) VALUES (:topic_id, :created_by, :threadname, NOW());"
+    sql = "INSERT INTO threads(topic_id, created_by, threadname, created_at, name_modified, visible) VALUES (:topic_id, :created_by, :threadname, NOW(), 0, 1);"
     db.session.execute(sql, {"topic_id":topic_id, "created_by":user, "threadname":threadname})
     db.session.commit()
     sql2 = "SELECT id FROM threads WHERE topic_id=:topic_id AND created_by=:created_by AND threadname=:threadname ORDER BY created_at DESC;"
     return db.session.execute(sql2, {"topic_id":topic_id, "created_by":user, "threadname":threadname}).fetchone()
+
+def edit_title(thread_id, title):
+    sql = "UPDATE threads SET threadname=:title, name_modified = 1 WHERE id=:thread_id"
+    db.session.execute(sql, {"title":title, "thread_id":thread_id})
+    db.session.commit()
+
+def delete_thread(thread_id):
+    sql = "UPDATE threads SET visible = 0 WHERE id=:thread_id"
+    db.session.execute(sql, {"thread_id":thread_id})
+    db.session.commit()
+
+def thread_visible(thread_id):
+    sql = "SELECT visible FROM threads WHERE id=:thread_id"
+    return db.session.execute(sql, {"thread_id":thread_id}).fetchone()
