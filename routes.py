d1422a7e674dcaf1805b42afe1d3b9e04fd6bcc1
oskarruo <oskar.ruokolainen@gmail.com>
2022-05-08 18:29:50 +0300

    added voting

--- routes.py
+++ routes.py
@@ -46,31 +46,36 @@ def create_topic():
         return render_template("create_topic.html")
     
     if request.method == "POST":
+        users.check_csrf()
         topic = request.form["title"]
         topics.create_topic(topic)
-        return redirect("/"+str(topic))
+        return redirect("/")
 
-@app.route("/<topic>")
-def topic(topic):
-    if topics.topic_visible(topic).visible < 1:
+@app.route("/<topic_id>")
+def topic(topic_id):
+    try:
+        if topics.topic_visible(topic_id).visible < 1:
+            abort(404)
+    except:
         abort(404)
-    allthreads = threads.get_threads(topic)
-    return render_template("threads.html", threads=allthreads, topic=topic)
+    allthreads = threads.get_threads(topic_id)
+    return render_template("threads.html", threads=allthreads, topic=topic_id)
 
-@app.route("/<topic>/delete")
-def topic_delete(topic):
+@app.route("/<topic_id>/delete")
+def topic_delete(topic_id):
     users.role_required(2)
-    topics.delete_topic(topic)
+    topics.delete_topic(topic_id)
     return redirect("/")
 
-@app.route("/<topic>/create_thread", methods=["POST", "GET"])
-def createthread(topic):
+@app.route("/<topic_id>/create_thread", methods=["POST", "GET"])
+def createthread(topic_id):
     users.role_required(1)
 
     if request.method == "GET":
-        return render_template("create_thread.html", topic=topic)
+        return render_template("create_thread.html", topic_id=topic_id)
 
     if request.method == "POST":
+        users.check_csrf()
         user = session["user_id"]
         threadname = request.form["title"]
         if len(threadname) < 1 or len(threadname) > 50:
@@ -78,8 +83,7 @@ def createthread(topic):
         message = request.form["message"]
         if len(message) < 1 or len(message) > 5000:
             return render_template("error.html", errormessage="Viestin tulee olla vähintään 1 merkin ja enintään 5000 merkin pituinen")
-        topic_id = topics.get_topicid(topic)
-        thread_id = threads.create_thread(topic_id.id, user, threadname)
+        thread_id = threads.create_thread(topic_id, user, threadname)
         messages.send_message(thread_id.id, message, user)
         return redirect("/thread/"+str(thread_id.id))
 
@@ -89,8 +93,8 @@ def thread(thread_id):
         abort(404)
     allmessages = messages.get_messages(thread_id)
     threadinfo = threads.get_threadinfo(thread_id)
-    topicname = topics.get_topic_name(thread_id)
-    return render_template("thread.html", messages=allmessages, info=threadinfo, topicname=topicname)
+    topic_id = topics.get_topic_id(thread_id)
+    return render_template("thread.html", messages=allmessages, info=threadinfo, topic_id=topic_id)
 
 @app.route("/thread/<thread_id>/reply", methods=["POST", "GET"])
 def reply(thread_id):
@@ -101,6 +105,7 @@ def reply(thread_id):
         return render_template("reply.html", info=threadinfo)
 
     if request.method == "POST":
+        users.check_csrf()
         content = request.form["content"]
         if len(content) < 1 or len(content) > 5000:
             return render_template("error.html", errormessage="Viestin tulee olla vähintään 1 merkin ja enintään 5000 merkin pituinen")
@@ -118,6 +123,7 @@ def edit(message_id):
         return render_template("edit.html", content=content, thread_id=thread_id)
 
     if request.method == "POST":
+        users.check_csrf()
         content = request.form["content"]
         if len(content) < 1 or len(content) > 5000:
             return render_template("error.html", errormessage="Viestin tulee olla vähintään 1 merkin ja enintään 5000 merkin pituinen")
@@ -144,6 +150,7 @@ def edit_title(thread_id):
         return render_template("edit_title.html", info = info)
 
     if request.method == "POST":
+        users.check_csrf()
         title = request.form["title"]
         if len(title) < 1 or len(title) > 50:
             return render_template("error.html", errormessage="Otsikon tulee olla vähintään 1 merkin ja enintään 50 merkin pituinen")
@@ -156,6 +163,17 @@ def delete_thread(thread_id):
         pass
     else:
         users.allow_thread_edit(thread_id)
-    topic = topics.get_topic_name(thread_id)
+    topic_id = topics.get_topic_id(thread_id)
     threads.delete_thread(thread_id)
-    return redirect("/"+str(topic.topic))
+    return redirect("/"+str(topic_id.id))
+
+@app.route("/<message_id>/upvote")
+def upvote(message_id):
+    thread_id = messages.get_thread_id(message_id)
+    if messages.has_voted(message_id, session["user_id"]):
+        pass
+    elif not users.role():
+        pass
+    else:
+        messages.vote(message_id, session["user_id"])
+    return redirect("/thread/"+str(thread_id.thread_id))
