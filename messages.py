05a9eb1bcfc34e3c6c57f302cd8d6021d2e3a9b9
oskarruo <oskar.ruokolainen@gmail.com>
2022-05-08 18:28:20 +0300

    added voting

--- messages.py
+++ messages.py
@@ -1,7 +1,7 @@
 from db import db
 
 def get_messages(thread_id):
-    sql = "SELECT messages.sent_by, messages.id, users.username, messages.content, messages.sent_at, messages.modified FROM users, messages WHERE messages.sent_by = users.id AND messages.thread_id =:thread AND messages.visible > 0 ORDER BY messages.sent_at, messages.id;"
+    sql = "SELECT messages.sent_by, messages.id, users.username, messages.content, messages.sent_at, messages.modified, count(votes) FROM users, messages LEFT JOIN votes ON messages.id = votes.message_id WHERE messages.sent_by = users.id AND messages.thread_id=:thread AND messages.visible > 0 GROUP BY messages.sent_by, messages.id, users.username ORDER BY messages.sent_at, messages.id;"
     return db.session.execute(sql, {"thread":thread_id}).fetchall()
 
 def send_message(thread_id, content, user_id):
@@ -26,3 +26,16 @@ def delete_message(message_id):
 def get_thread_id(message_id):
     sql = "SELECT thread_id FROM messages WHERE id=:message_id;"
     return db.session.execute(sql, {"message_id":message_id}).fetchone()
+
+def has_voted(message_id, user_id):
+    sql = "SELECT * from votes WHERE message_id=:message_id AND vote_by=:user_id;"
+    result = db.session.execute(sql, {"message_id":message_id, "user_id":user_id}).fetchone()
+    if result == None:
+        return False
+    else:
+        return True
+
+def vote(message_id, user_id):
+    sql = "INSERT INTO votes (message_id, vote_by) VALUES (:message_id, :user_id);"
+    db.session.execute(sql, {"message_id":message_id, "user_id":user_id})
+    db.session.commit()
